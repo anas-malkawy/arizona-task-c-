3aba38f7dbe25c750f8d07ebd37ea475264e47b4
anasmalkawy <anas.malkawy1999@gmail.com>
2025-07-09 15:11:05 +0300

    Add project files.

---
+++ task arizona 1/Program.cs
@@ -0,0 +1,48 @@
+﻿Random r = new Random();
+int rInt = r.Next(0, 100);
+
+int a = 0;
+
+while (true)
+{
+    a++;
+
+    Console.WriteLine("Enter a number: ");
+
+    string i = Console.ReadLine();
+
+    if (i.ToLower() == "quit")
+    {
+        Console.WriteLine("You have exited the game");
+        Console.WriteLine($"You have tried {a} times");
+
+        break;
+    }
+
+    bool succedParse = int.TryParse(i, out int num1); // Fixed TryParse to include the 'out' parameter
+    if (!succedParse)
+    {
+        Console.WriteLine("Invalid input. Please enter a valid number.");
+        continue;
+    }
+
+    if (num1 > rInt)
+    {
+        Console.WriteLine("higher");
+    }
+    else if (num1 < rInt)
+    {
+        Console.WriteLine("less");
+    }
+    else
+    {
+        Console.WriteLine("equal");
+    }
+
+    if (num1 == rInt)
+    {
+        Console.WriteLine($"You have tried {a} times");
+
+        break;
+    }
+}
---
+++ task arizona 2/Program.cs
@@ -0,0 +1,112 @@
+﻿
+public record member(string name, int memberID);
+
+public interface ILoanable
+{
+    Task CheckoutAsync(member m);
+    Task ReturnAsync();
+}
+
+public abstract class LibraryItem : ILoanable
+{
+    public virtual async Task CheckoutAsync(member m)
+    {
+    }
+
+    public virtual async Task ReturnAsync()
+    {
+    }
+}
+
+public struct ISBN
+{
+    private readonly string _num;
+
+    public ISBN(string num)
+    {
+        _num = num;
+    }
+
+}
+
+public class Book : LibraryItem
+{
+    public static Library Library { get; set; } = new Library();
+
+    public override async Task CheckoutAsync(member m)
+    {
+        Library.AddItem(this);
+        await Task.Delay(3000);
+        Console.WriteLine("Book checked out.");
+    }
+
+    public override async Task ReturnAsync()
+    {
+        Console.WriteLine("Book returned.");
+    }
+}
+
+
+
+public class Library
+{
+
+    private List<ILoanable> _items = new List<ILoanable>();
+
+    public Dictionary<ILoanable, member> _track = new Dictionary<ILoanable, member>();
+
+    public void AddItem(ILoanable item)
+    {
+        _items.Add(item);
+    }
+
+    public void trackP(ILoanable item, member m)
+    {
+        _track.Add(item, m);
+    }
+
+}
+
+
+public class CheckoutSession : IAsyncDisposable
+{
+
+    private List<ILoanable> _items = new List<ILoanable>();
+
+    public void AddItem(ILoanable item)
+    {
+        _items.Add(item);
+    }
+    public async Task CheckoutItem(member m)
+    {
+        foreach (var item in _items)
+        {
+            await item.CheckoutAsync(m);
+        }
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        foreach (var item in _items)
+        {
+            await item.ReturnAsync();
+        }
+    }
+
+}
+
+public class Program
+{
+    public static async Task Main()
+    {
+        Book book1 = new Book();
+        member member1 = new member("Anas", 1);
+
+        await using var session = new CheckoutSession();
+        session.AddItem(book1);
+
+        await session.CheckoutItem(member1);
+
+        Console.WriteLine("Session ended, items returned.");
+    }
+}
